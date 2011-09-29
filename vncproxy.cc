fc2b059e0d4985fcd64348ef410167b964cd15c3
Santa Zhang <santa1987@gmail.com>
2011-09-29 10:56:29 +0800

    add more autotools

--- vncproxy.cc
+++ vncproxy.cc
@@ -1,12 +1,15 @@
 #include <iostream>
+#include <pthread.h>
 
 #include "config.h"
+
 #include "d3des.h"
 
 using namespace std;
+
+pthread_mutex_t vnc_mapping_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 int main(int argc, char* argv[]) {
     cout << "This shall be done!" << endl;
     return 0;
 }
-
